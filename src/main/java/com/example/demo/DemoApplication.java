73d310ae03ccb812100c98084742a32d493d7b7c
dmori <chosehoon2@naver.com>
2025-07-17 14:08:44 +0900

    예제 1.11 spring.factories로 ApplicationListener 등록

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.listener.ApplicationStartingEventListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(DemoApplication.class);
-        springApplication.addListeners(new ApplicationStartingEventListener());
         springApplication.run(args);
     }
 
